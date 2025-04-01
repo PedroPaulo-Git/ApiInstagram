f702ccea292dbca1fb8300d81c0ae0172871aebb
Pedro Paulo da Silva Monteiro <109928508+PedroPaulo-Git@users.noreply.github.com>
2025-04-01 08:35:24 -0300

    backend to working in new domain

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ app.use(
       "http://localhost:3000",
       "https://espiafacil.com.br",
       "https://espiafacil.info",
+      "https://esp1afacil.shop",
     ],
     methods: ["GET"],
     allowedHeaders: ["Content-Type"],
